9870b5b58ff91d36501bf8949bfc5405f82edbfa
Jeison Ferrer <jeison120120@hotmail.com>
2025-06-11 17:26:54 -0500

    server configurado

---
+++ src/server/server.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { config } from 'dotenv';
+
+//import { router as pinRouter  } from '../servicios/pin/pinroutes'; // Asegúrate de que la ruta sea correcta
+
+config(); // Cargar variables de entorno desde el archivo .env
+
+class Server {
+    private port: number;
+    private app: express.Application;
+    //private server: http.Server; //Este funciona para ws desde arduino.
+
+    constructor() {
+        
+        this.port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+        this.app = express();
+
+        //this.server = http.createServer(this.app);
+
+    }
+
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.static(path.join(__dirname, '../../public')));
+    }
+    
+    routes() {
+        //this.app.use('/api', pinRouter);
+    }
+    start() {
+        this.middlewares();
+        this.routes();
+
+        this.app.listen(this.port, () => {
+            console.log(`Server is running at port ${this.port}`);
+        }  );
+
+
+    }
+}
+
+export const server = new Server(); 

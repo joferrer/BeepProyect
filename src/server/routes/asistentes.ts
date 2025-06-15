bcea393785cab8dccad8586b796c25274d321e68
Jeison Ferrer <jeison120120@hotmail.com>
2025-06-15 12:01:20 -0500

    ruta para registrar un nuevo asistente

--- src/server/routes/asistentes.ts
+++ src/server/routes/asistentes.ts
@@ -1,4 +1,4 @@
-import { obtenerAsistentes } from "../../firebase/db";
+import { obtenerAsistentes, registrarAsistente } from "../../firebase/db";
 import { Router } from "express";
 
 export const asistentesRouter = Router();
@@ -12,3 +12,24 @@ asistentesRouter.get("/asistentes", async (_req, res) => {
     res.status(500).json({ error: "Error al obtener asistentes" });
   }
 });
+
+asistentesRouter.post("/asistente", async (req, res) => {
+    const persona = req.body;
+    
+    if (!persona || !persona.NOMBRE || !persona.APELLIDO) {
+         res.status(400).json({ error: "Datos incompletos" });
+         return;
+    }
+    
+    try {
+        const registrado = await registrarAsistente(persona);
+        if (registrado) {
+        res.status(201).json({ message: "Asistente registrado exitosamente" });
+        } else {
+        res.status(500).json({ error: "Error al registrar asistente" });
+        }
+    } catch (error) {
+        console.error("Error al registrar asistente:", error);
+        res.status(500).json({ error: "Error al registrar asistente" });
+    }
+})

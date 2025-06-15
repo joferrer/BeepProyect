e7c1b5c1388d6d5177b3cdcf0b7cebf13d9bbf2e
Jeison Ferrer <jeison120120@hotmail.com>
2025-06-14 20:26:21 -0500

    server configurado

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { hacerPreregistro } from './firebase/preregistro';
+//import { hacerPreregistro } from './firebase/preregistro';
 import {server} from './server/server';
 //import { hacerPreregistro } from './xlsx/preregistro';
 
 server.start(); // Iniciar el servidor
 
-hacerPreregistro() // Hacer el prerregistro
+//hacerPreregistro() // Hacer el prerregistro
     //.then(() => console.log('Preregistro completado'))
